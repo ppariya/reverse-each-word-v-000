271a908dc8bc07614b03b163b66ebd7a4f31006c
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-02 15:11:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   array = [sentence.split]
   reverse = []
   array.each do |word|
-  split = word.split
-  word = split.reverse
+  split = [word.split]
+  word = [split.reverse]
   reverse << word.join
   end
   reverse
